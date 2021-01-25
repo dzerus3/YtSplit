45f88e924d13f128f40f4979279912f6b43714a5
Dellon Zerus <dellonzerus@gmail.com>
2021-01-24 20:57:55 -0800

    Removed --intelligent option
    It was an incredibly convoluted solution to the problem which I was
    trying to fix, and it simply didn't work. I do not want to try and
    fix this at the moment as I believe --pad does the job well enough,
    so I'm just gonna move all related code to a separate branch until
    later.

--- splitter.py
+++ splitter.py
@@ -115,7 +115,6 @@ class VideoManipulator:
 
         self.timestamps = timestamps
         self.videoName = downloader.getVideo()
-        self.timestampManip.loadVideoSilences(self.videoName)
         self.fileFormat = self.getFileFormat(self.videoName)
 
     def splitVideo(self):
@@ -132,7 +131,6 @@ class VideoManipulator:
             dbg_print("Timestamp before padding: " + currentTime + "  " + endTime)
             processedStart, processedEnd = self.timestampManip.padTimestamps(currentTime, endTime)
             dbg_print("Timestamp after padding: " + processedStart + "  " + processedEnd)
-            processedStart, processedEnd = self.timestampManip.silenceSplit(processedStart, processedEnd)
 
             #TODO make this work
             # command = ["ffmpeg", "-ss", currentTime, "-t", endTime, "-i", videoName, "-acodec", "copy", "-vcodec", "copy", "\"" + name + "." + fileFormat + "\""]
@@ -176,11 +174,6 @@ class VideoManipulator:
         return re.sub("'|\n", "", duration)
 
 class TimestampManipulator:
-    def loadVideoSilences(self, fileName):
-        if arguments.intelligent:
-            silence = SilenceFinder()
-            self.silenceTimestamps = silence.getSilenceTimestamps(fileName)
-
     def getStartingTime(self, firstTimestamp):
         if arguments.zero:
             return "0:00"
@@ -214,28 +207,6 @@ class TimestampManipulator:
 
         return self.convertFromSeconds(startSeconds), self.convertFromSeconds(endSeconds)
 
-    def silenceSplit(self, segmentStart, segmentEnd):
-        if arguments.intelligent:
-            startSeconds = self.convertToSeconds(segmentStart)
-            endSeconds = self.convertToSeconds(segmentEnd)
-
-            # Loops through every silence timestamp ffmpeg detected and checks
-            # whether it is more than arguments.intelligent_time seconds away
-            # from a given timestamp
-            for stamp in self.silenceTimestamps:
-                difference = abs(startSeconds - stamp[1])
-                if difference < arguments.intelligent_time:
-                    segmentStart = self.convertFromSeconds(stamp[1])
-                    break
-
-            for stamp in self.silenceTimestamps:
-                difference = abs(endSeconds - stamp[0])
-                if difference < arguments.intelligent_time:
-                    segmentEnd = self.convertFromSeconds(stamp[0])
-                    break
-
-        return segmentStart, segmentEnd
-
     # Could not find any built-in method to do what I want, so here we go
     def convertToSeconds(self, timeString):
         seconds = 0
@@ -253,63 +224,6 @@ class TimestampManipulator:
         delta = timedelta(seconds=seconds)
         return str(delta)
 
-class SilenceFinder:
-    def getSilenceTimestamps(self, fileName):
-        dbg_print("Retrieving silent parts of video for --intelligent. This may take a while.")
-        cmd = f"ffmpeg -i {fileName} -af silencedetect=d=0.5:n=0.03 -f null -"
-        args = cmd.split()
-        # FFMPEG only properly output to subprocess.Popen due to the way it buffers lines
-        process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
-        # Process runs parallel to ytsplit, so wait for it to finish first
-        process.wait()
-
-        silence = self.readSilenceTimestamps(process.stdout.read().splitlines())
-
-        return silence
-
-    def readSilenceTimestamps(self, output):
-        silence = []
-        pair = [None, None]
-
-        for line in output:
-            start = self.checkStartRegex(line)
-            end = self.checkEndRegex(line)
-
-            if start:
-                pair[0] = float(start)
-            elif end:
-                pair[1] = float(end)
-
-            if pair[0] and pair[1]:
-                silence.append(pair)
-                pair = [None, None]
-
-        return silence
-
-    def checkStartRegex(self, line):
-        startRegex = "(?<=silence_start:\ )\d*\.?\d*"
-        silenceStart = 0
-
-        match = re.search(startRegex, line)
-
-        if match:
-            time = match.group(0)
-            return time
-        else:
-            return None
-
-    def checkEndRegex(self, line):
-        endRegex = "(?<=silence_end:\ )\d*\.?\d*"
-        silenceStart = 0
-
-        match = re.search(endRegex, line)
-
-        if match:
-            time = match.group(0)
-            return time
-        else:
-            return None
-
 def parseArgs():
     parser = argparse.ArgumentParser(
         description="YtSplit - Splits a file based on Youtube timestamps.",
@@ -324,10 +238,6 @@ def parseArgs():
         "pad": "Pad beginning and end of audio by certain amount of seconds. May help smooth transitions when splitting music compilations.",
         "pad_beginning": "Same as -p, but only for the beginning.",
         "pad_end": "Same as -p, but only for the end.",
-        "intelligent": "Attempts to intelligently detect transitions between tracks based on whether there is silence next to the timestamp. Not guaranteed to work.",
-        "intelligent_duration": "Minimum duration of silence for it to be noticed. Usable only with --intelligent. Default: 0.5",
-        "intelligent_sensitivity": "Sensitivity of silence detector. Higher means more sensitive. Do not set the value too high. Usable only with --intelligent. Default: 0.03",
-        "intelligent_time": "How many seconds ahead and behind the timestamp silence detector will search. Usable only with --intelligent. Default: 3",
         "zero": "Start with the first timestamp at 00:00. Useful if the first timestamp is not there.",
         "download": "Downloads video from youtube (requires youtube-dl)",
         "format": "Specify output format for downloaded video (only for download)",
@@ -369,29 +279,6 @@ def parseArgs():
                         action="store",
                         dest="pad_end",
                         help=help_texts["pad_beginning"])
-
-    parser.add_argument("-i", "--intelligent",
-                        action="store_true",
-                        dest="intelligent",
-                        help=help_texts["intelligent"])
-
-    parser.add_argument("-id", "--intelligent-duration",
-                        action="store_true",
-                        dest="intelligent_duration",
-                        default=0.5,
-                        help=help_texts["intelligent_duration"])
-
-    parser.add_argument("-is", "--intelligent-sensitivity",
-                        action="store_true",
-                        dest="intelligent_sensitivity",
-                        default=0.3,
-                        help=help_texts["intelligent_sensitivity"])
-
-    parser.add_argument("-it", "--intelligent-time",
-                        action="store_true",
-                        dest="intelligent_time",
-                        default=3,
-                        help=help_texts["intelligent_time"])
 
     parser.add_argument("-n", "--numerical",
                         action="store_true",
